9123c99e36e82d149fcc3dd549c9e2666e00051d
Fantor2 <113799893+Fantor2@users.noreply.github.com>
2022-11-06 13:21:32 +0300

    Add files via upload

---
+++ 06-11.py
@@ -0,0 +1,125 @@
+##n=int(input())
+##lst= []
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##    if lst[i]%2==0:
+##        print(lst[i], end = " ")
+
+
+
+
+
+
+
+
+
+
+##n=int(input())
+##lst= []
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##     if i%2==0:
+##         print(lst[i], end = ' ')
+
+
+
+
+
+
+##n=int(input())
+##lst= []
+##s=0
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##    s+=lst[i]
+##print(s, end = ' ')
+
+
+
+
+
+
+
+##n=int(input())
+##lst= []
+##s=1
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##    s*=lst[i]
+##print(s, end = ' ')
+
+
+
+
+
+
+
+
+
+
+
+##n=int(input())
+##lst= []
+##s=0
+##k=0
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##   s+=lst[i]
+##m= s/n
+##print(m)
+
+
+
+
+
+
+
+
+
+##import math
+##n=int(input())
+##lst= []
+##s=1
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##   s*=lst[i]
+##m=math.sqrt(s)
+##print(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##найти количесто тех чисел что делятся на 3 и не делятся на 5
+##n=int(input())
+##lst= []
+##s=1
+##for i in range (n):
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##    if lst[i]%3==0 and lst[i]%5!=0:
+        
+    
